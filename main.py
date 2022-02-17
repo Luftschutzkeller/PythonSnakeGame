c6d5bdcdb8c9722fbbb3c565e87fddad6ae2213f
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2022-02-17 16:02:39 +0300

    first screen

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pygame
 
+size = [400, 600]
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('SNAKE')
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('exit')
+            pygame.quit()
